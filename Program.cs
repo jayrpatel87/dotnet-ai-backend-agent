7a749a3e2f89c19e0d9029d5afe086b32260650c
Jay <jay31846@gmail.com>
2026-03-15 01:35:15 -0400

    Fixed the imports

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+#pragma warning disable SKEXP0070
+
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+//using Microsoft.SemanticKernel.ChatCompletion.OllamaKernelBuilderExtensions;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<Kernel>(sp =>
 {
     var kerneBuilder = Kernel.CreateBuilder();
-    kerneBuilder.AddOllamaChatCompletion("Ollama", "http://localhost:11434");
+    kerneBuilder.AddOllamaChatCompletion(modelId : "phi3", endpoint: new Uri("http://localhost:11434"));
 
     return kerneBuilder.Build();
 });
@@ -18,7 +22,7 @@ app.MapGet("/chat", async (ChatRequest request, Kernel kernel) =>
 {
     var chatService = kernel.GetRequiredService<IChatCompletionService>();
 
-    var result = await chatService.GetChatMessageCompletionsAsync(
+    var result = await chatService.GetChatMessageContentAsync(
         "You are a helpful senior .NET backend engineer with deep SQL Server knowledge. \n\nUser: " + request.Query);
 
     return Results.Ok(new { response = result.Content, model = "phi3" });
